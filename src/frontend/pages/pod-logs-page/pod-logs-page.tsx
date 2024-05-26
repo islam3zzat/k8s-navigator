d5afb53f2ece93e9e37dd5d28ac0262366efc435
Islam farg <islam.farg@commercetools.com>
2024-05-27 00:05:37 +0100

    add log icon

--- src/frontend/pages/pod-logs-page/pod-logs-page.tsx
+++ src/frontend/pages/pod-logs-page/pod-logs-page.tsx
@@ -1,12 +1,39 @@
-import { useCallback, useEffect, useState } from "react";
+import InfoIcon from "@mui/icons-material/Info";
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+import WarningAmberIcon from "@mui/icons-material/WarningAmber";
+import { ChangeEvent, useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Button from "@mui/material/Button";
 import FileDownloadIcon from "@mui/icons-material/FileDownload";
 import Stack from "@mui/material/Stack";
+import Radio from "@mui/material/Radio";
+import FormControlLabel from "@mui/material/FormControlLabel";
 import Typography from "@mui/material/Typography";
 import ArticleIcon from "@mui/icons-material/Article";
 import Paper from "@mui/material/Paper";
 import { useAppContext } from "../../app-context";
+
+type Severity = "error" | "warning" | "info";
+const logToSeverity = (log: string): Severity => {
+  if (log.includes("ERROR")) {
+    return "error";
+  }
+  if (log.includes("WARN")) {
+    return "warning";
+  }
+  return "info";
+};
+
+const LogIcon = ({ severity }: { severity: Severity }) => {
+  switch (severity) {
+    case "error":
+      return <ErrorOutlineIcon sx={{ mt: 1.5 }} color="error" />;
+    case "warning":
+      return <WarningAmberIcon sx={{ mt: 1.5 }} color="warning" />;
+    default:
+      return <InfoIcon sx={{ mt: 1.5 }} />;
+  }
+};
 
 const PodLogsPage = () => {
   const { podName, containerName } = useParams<{
@@ -14,6 +41,19 @@ const PodLogsPage = () => {
     containerName: string;
   }>();
   const { state, dispatch } = useAppContext();
+  const [selectedValue, setSelectedValue] = useState<Severity | "all">("all");
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSelectedValue(event.target.value as Severity | "all");
+  };
+
+  const controlProps = (item: string) => ({
+    checked: selectedValue === item,
+    onChange: handleChange,
+    value: item,
+    name: "log-severity-radio-button",
+    inputProps: { "aria-label": item },
+  });
 
   useEffect(() => {
     const path = `/pods/${podName}/logs/${containerName}`;
@@ -31,12 +71,7 @@ const PodLogsPage = () => {
       });
     }
   }, [state.breadCrumbs, dispatch, podName, containerName]);
-  //   window.logs.onLogEnd(() => {console.log("done!")})
-  //   window.logs.onLogError(err => {console.log("got error", err)})
-  //   window.logs.onLogData(console.log)
-  //   window.logs.tailPodLogs("import-api", "test-import-api-resequencer-59f669985c-tzv5z", "resequencer", true)
-  //   window.logs.stopLogStream("import-api", "test-import-api-resequencer-59f669985c-tzv5z", "resequencer")
-  // setup log streaming hooks
+
   const [logs, setLogs] = useState<string[]>([]);
   useEffect(() => {
     if (!state.activeNamespace) {
@@ -92,20 +127,51 @@ const PodLogsPage = () => {
       {/* Sticky position */}
       <Paper elevation={2} sx={{ p: 2 }}>
         <Stack spacing={2}>
-          {logs.map((log: string, index: number) => (
-            <Typography
-              sx={{
-                p: 1,
-                textWrap: "wrap",
-                mb: 2,
-                width: "min(max-content, 100%)",
-                lineBreak: "anywhere",
-              }}
-              key={index}
-            >
-              {log}
-            </Typography>
-          ))}
+          <Stack alignSelf="center" direction="row" spacing={2}>
+            <FormControlLabel
+              value="all"
+              control={<Radio {...controlProps("all")} />}
+              label="All"
+            />
+            <FormControlLabel
+              value="info"
+              control={<Radio color="info" {...controlProps("info")} />}
+              label="Info"
+            />
+            <FormControlLabel
+              value="warning"
+              control={<Radio color="warning" {...controlProps("warning")} />}
+              label="Warning"
+            />
+            <FormControlLabel
+              value="error"
+              control={<Radio color="error" {...controlProps("error")} />}
+              label="Error"
+            />
+          </Stack>
+          {logs.map((log: string, index: number) => {
+            const severity = logToSeverity(log);
+            if (selectedValue !== "all" && selectedValue !== severity) {
+              return null;
+            }
+
+            return (
+              <Stack key={index} direction="row" spacing={0}>
+                <LogIcon severity={severity} />
+                <Typography
+                  sx={{
+                    p: 1,
+                    textWrap: "wrap",
+                    mb: 2,
+                    width: "min(max-content, 100%)",
+                    lineBreak: "anywhere",
+                  }}
+                >
+                  {log}
+                </Typography>
+              </Stack>
+            );
+          })}
         </Stack>
       </Paper>
     </Stack>
