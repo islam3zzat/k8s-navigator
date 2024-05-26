4d4e820a0d052bd6a751753faccec4f0f1d0d1a4
Islam farg <islam.farg@commercetools.com>
2024-05-26 23:28:41 +0100

    Fix wrong label

--- src/frontend/pages/home-page/home-page.tsx
+++ src/frontend/pages/home-page/home-page.tsx
@@ -73,7 +73,7 @@ const HomePage = () => {
       </Helmet>
       <Stack spacing={2}>
         <Stack direction="row" spacing={2} alignItems="center">
-          <Typography variant="h6">Cluster:</Typography>
+          <Typography variant="h6">Context:</Typography>
           <Typography variant="body1">{state.activeContext?.name}</Typography>
         </Stack>
         <Stack direction="row" flexWrap="wrap" gap={1}>
